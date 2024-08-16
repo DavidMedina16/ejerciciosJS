3a39951d45aaf9c9c7ad873bfa66335b41f09a74
David <davidparramedina@hotmail.com>
2024-08-16 16:18:29 -0500

    ejercicio 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,6 +1,16 @@
 /*Realizar una tabla de multiplicación de un número (a) desde 0 hasta x, pero que no se muestre la multiplicación de a * 5.*/
-function tablaMultiplicacion (a = 0 , x = 12) { /*creamos la funcion y le damos los paramatros */
-    let  resultado = [];
+function tablaMultiplicacion (a = 0 , x = 0) { /*creamos la funcion y le damos los paramatros */
+    let  tabla = []; /*creamos el arreglo vacio en el cual se almacenaran los datos de la tabla*/
 
-    for (  )
+    for ( let i = 0; i <= x; i++ ) { /*Iniciamos un ciclo desde 0 hasta 'x' */
+        if ( i === 5 ) { /*demos la indicacion de condicionamiento de que si el multiplicador es 5*/
+            continue; /* salte este paso del bucle */
+        }
+        let resultado = a * i; /*Realizamos la multiplicacion de a por i*/
+        tabla.push(`${a} * ${i} = ${resultado}`);
+    }
+    return tabla; /*Devolvemos el arreglo con los resultados almaacenados*/
 }
+
+/*Uso*/
+console.log(tablaMultiplicacion(1,10).join('\n'));
